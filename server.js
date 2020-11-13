6848b8dc24c47dcf8ac1e2519547db267f357359
riesvile <vilem@ries.cz>
2020-11-13 23:14:24 +0100

    test1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ wss.on('connection', function connection(ws) {
 	ws.send('welcome!');
 	ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-	for (i = 0; i < 100000000; i++){
+	for (i = 0; i < 1000000; i++){
 		x = i ** 2;
 	}
 	ws.send('x is ' + x + ' and your message: ' + message);
@@ -32,7 +32,7 @@ wss.on('connection', function connection(ws) {
 
 
 
-app.get('/', (req, res) => res.sendFile('./index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 
 server.listen(3000, () => console.log('Listening on port :3000'))
