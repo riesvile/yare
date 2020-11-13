6a16d226cdb6150c909d26bd94da98f100fdf05a
riesvile <vilem@ries.cz>
2020-11-13 16:44:50 +0100

    intro

---
+++ logger.js
@@ -0,0 +1,21 @@
+
+var url = 'http://';
+
+
+
+class Spirit {
+	constructor(){
+		this.position =
+		this.energy_capacity =
+		this.energy =
+		this.hp =
+		this.move_speed = 
+	}
+	
+	
+}
+
+
+var spirit = new Spirit();
+
+console.log(spirit);
---
+++ server.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+
+
+const wss = new WebSocket.Server({ server });
+
+x = 0;
+
+
+wss.on('connection', function connection(ws) {
+	console.log('new client connected');
+	ws.send('welcome!');
+	ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+	for (i = 0; i < 100000000; i++){
+		x = i ** 2;
+	}
+	ws.send('x is ' + x + ' and your message: ' + message);
+	
+  });
+
+  ws.send('something');
+});
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => res.send('Hello there!'))
+
+
+server.listen(3000, () => console.log('Listening on port :3000'))
