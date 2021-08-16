5308e8cb6eefc65a91799f2f8d1a448345ad5135
swz-gh <contact@swz.works>
2021-08-16 15:24:50 +0200

    remove comment

--- server.js
+++ server.js
@@ -836,7 +836,6 @@ function discord_postmessage(hook, msg){
 
 async function discord_automatch_bot(usr){
   let matchingusers = await User.find({user_id: usr})
-  console.log("Posted to discord:", matchingusers, matchingusers.length === 0)
   if (matchingusers.length === 0) return;
 	discord_postmessage(config.hooks.queue, usr + ' is waiting in the queue');
 }
