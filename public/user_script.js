c1ba2b2d2f337c0bd4b83a8e6ea5c4ae46ff2238
riesvile <vilem@ries.cz>
2022-02-10 17:00:28 +0100

    -

--- public/user_script.js
+++ public/user_script.js
@@ -1,6 +1,8 @@
 
 function test_click(){
 	client_object['attack'] = 'working';
+	//alert('w');
+	update_code();
 }
 
 
@@ -9,7 +11,7 @@ var plate = document.getElementById("modules_plate");
 
 var attack_button = document.createElement("div");
 plate.appendChild(attack_button);
-attack_button.innerHTML = "<div style='background-color: red; width: 80px; height: 80px; position: absolute; top: 200px; right: 200px;' onclick='test_click'>TEST</div>";
+attack_button.innerHTML = "<div style='background-color: red; width: 80px; height: 80px; position: absolute; top: 200px; right: 200px;' onclick='test_click()'>TEST</div>";
 
 //attack_button.addEventListener("click", test_click, false);
 
